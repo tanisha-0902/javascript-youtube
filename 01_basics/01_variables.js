e3f63d1706a81c94a3b6caef5252b434fa58337b
tanisha-0902 <159106415+tanisha-0902@users.noreply.github.com>
2025-07-16 19:06:05 +0000

    variables studied

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="tanisha@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+//accountId=2//not allowed cant change const value
+accountEmail="tc@tc.com"//changedd
+accountPassword="123456"//changed
+accountCity="Bengaluru"//changed
+console.log(accountId);
+console.log([accountId,accountEmail,accountPassword,accountCity])//to print multiple things at once
+// prefer not to use var because of issue in block scope and functional scope
