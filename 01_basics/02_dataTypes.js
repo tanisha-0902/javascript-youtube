080b3187ec136d5deaf613f0b94f6ea904bc31bb
tanisha-0902 <159106415+tanisha-0902@users.noreply.github.com>
2025-07-16 19:24:21 +0000

    datatypes studied

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3) //we are using nodejs,not browser
+console.log(3+3); console.log("tanisha")//can be written like this but dosent makes your code look good
+//datatypes-
+let name="tanisha"//string
+let age=18//int
+let isLoggedIn=false//boolean
+
+//PRIMITIVE DATATYPES
+//number range(2 to power 53)
+//bigint
+//string(can be used in '' or "")
+//boolean(true/false)
+//null(standalone value)(empty) its type is object
+//undefined(value not defined but variable is declared (eg:-let state;))
+//symbol(unique)
+
+//NON-PRIMITIVE
+//object
+console.log(typeof"tanisha"); 
+console.log(typeof undefined);//undefined
+console.log(typeof null); //object
