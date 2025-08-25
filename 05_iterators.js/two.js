14964df83e4972cbb364cfc83b48d281ab5d9caa
Tanisha <159106415+tanisha-0902@users.noreply.github.com>
2025-08-25 07:16:51 +0000

    loops

---
+++ 05_iterators.js/two.js
@@ -0,0 +1,22 @@
+//while loop
+
+let index = 0//initializing
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2//increment
+// }
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);//it will execute at least once
